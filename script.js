cd7fc625ef5687f7bd9ba3263163be5c80bf2e73
Muhammad Tifak <moht20009@gmail.com>
2019-12-09 20:13:29 -0800

    Integrated Google Maps

--- script.js
+++ script.js
@@ -63,3 +63,62 @@ jQuery(document).ready(function($) {
 
 
 TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+
+
+
+/*Google Maps
+**************************************************** */
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(37.7877052,-122.4131217);
+  var mapOptions = {
+    zoom: 18,
+    center: myLatlng
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var contentString =
+      '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h4>Make School</h4>'+
+      '<div id="bodyContent">'+
+      '<p>555 Post Street</br> San Francisco, CA 94102</p>'+
+      '<a href="https://www.google.com/maps/dir//Make+School,+555+Post+St,+San+Francisco,+CA+94102/@37.787701,-122.4131217,17z/data=!4m16!1m6!3m5!1s0x8085807f8bd4d441:0xd4618e6f27febe26!2sMake+School!8m2!3d37.787701!4d-122.410933!4m8!1m0!1m5!1m1!1s0x8085807f8bd4d441:0xd4618e6f27febe26!2m2!1d-122.410933!2d37.787701!3e2" target="_blank">Get Directions</a>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Civil'
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+/* Movement of questions for FAQ section
+***************************************************************** */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
